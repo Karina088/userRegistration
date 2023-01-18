27be277e3b554c7443c6ab97072b1c94c10c1e93
kary_ser <morskya.life@gmail.com>
2023-01-18 17:03:32 +0300

    compressed pictures

--- chatDesk/js/messageBtn.js
+++ chatDesk/js/messageBtn.js
@@ -3,5 +3,5 @@
 const profEl = document.querySelector('.profile__dropdown-list');
 document.querySelector('.profile__dropdown-toggle')
     .addEventListener('click', () => {
-        profEl.classList.toggle('show');
+        profEl.classList.toggle('showMessageBox');
     });
